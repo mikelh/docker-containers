f2293306ca32c0bafbc8594d8fe77f400e2ddafd
mikelh <mhasselberg@web.de>
2016-06-07 08:58:24 +0200

    Update vdr.sh
    add volume pre-seed

--- vdr-headless/vdr.sh
+++ vdr-headless/vdr.sh
@@ -13,6 +13,10 @@ export LANG="de_DE.UTF-8"
 [ "$(stat -c %u:%g /var/cache/vdr)" = "0:0" ] && chown vdr:vdr /var/cache/vdr
 [ "$(stat -c %u:%g /var/lib/vdr)" = "0:0" ] && chown vdr:vdr /var/lib/vdr
 
+# We need to preseed the volumes, just copy new files, no overwrite
+cp -a -v --no-clobber /var/lib/vdr/* /vdr/config
+# cp -a /var/lib/vdr/plugins/epg2vdr/epg.dat /vdr/config/plugins/epg2vdr
+
 # Run vdr
 # exec s6-setuidgid vdr vdr
 # vdr needs to start as root when using setpriority
